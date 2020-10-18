9777b28e439e70e2a6120c53913930b9d91d377a
Martin Lacroix <47146335+Martin-Lacroix@users.noreply.github.com>
2020-10-18 13:30:44 +0200

    Update mesh.h

--- vector/mesh.h
+++ vector/mesh.h
@@ -47,7 +47,6 @@ class Mesh{
 
     Mesh(vector<vector<double>> nXY,vector<vector<int>> eId);
     VectorXd dirichletBC2(VectorXd b,vector<int> nId,vector<double> bc,int dim);
-    SM jacobian(function<VectorXd(VectorXd)> fun,VectorXd &u,double dx);
     VectorXd neumannBC(vector<Face> &fList,vector<Vector2d> bc);
     vector<Face> setFace(vector<vector<int>> fId);
     SM dirichletBC1(SM A,vector<int> nId,int dim);
