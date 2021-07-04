e18a208ff1183e400f3b9474520a7053e0735530
Martin Lacroix <47146335+Martin-Lacroix@users.noreply.github.com>
2021-07-04 17:03:28 +0200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,6 +1,21 @@
 mkdir build
-g++ -O2 -fopenmp -I C:\Computing\eigen-3.4-rc1 vector\mechanics.cpp vector\mesh.cpp -o build\mechanics.exe
-g++ -O2 -fopenmp -I C:\Computing\eigen-3.4-rc1 scalar\advection.cpp scalar\mesh.cpp -o build\advection.exe
-g++ -O2 -fopenmp -I C:\Computing\eigen-3.4-rc1 scalar\transport.cpp scalar\mesh.cpp -o build\transport.exe
-g++ -O2 -fopenmp -I C:\Computing\eigen-3.4-rc1 scalar\diffusion.cpp scalar\mesh.cpp -o build\diffusion.exe
-g++ -O2 -fopenmp -I C:\Computing\eigen-3.4-rc1 scalar\laplace.cpp scalar\mesh.cpp -o build\laplace.exe
+
+:: External library paths
+
+set path-eigen=C:\Computing\msys64\mingw64\include\eigen3
+
+:: Main code paths
+
+set mechanics-cpp=vector\mechanics.cpp vector\mesh.cpp
+set advection-cpp=scalar\advection.cpp scalar\mesh.cpp
+set transport-cpp=scalar\transport.cpp scalar\mesh.cpp
+set diffusion-cpp=scalar\diffusion.cpp scalar\mesh.cpp
+set laplace-cpp=scalar\laplace.cpp scalar\mesh.cpp
+
+:: GCC Compiler
+
+g++ -O3 -fopenmp -I %path-eigen% %mechanics-cpp% -o build\mechanics.exe
+g++ -O3 -fopenmp -I %path-eigen% %advection-cpp% -o build\advection.exe
+g++ -O3 -fopenmp -I %path-eigen% %transport-cpp% -o build\transport.exe
+g++ -O3 -fopenmp -I %path-eigen% %diffusion-cpp% -o build\diffusion.exe
+g++ -O3 -fopenmp -I %path-eigen% %laplace-cpp% -o build\advection.exe
